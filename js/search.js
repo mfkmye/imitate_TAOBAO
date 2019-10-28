f56ef8df1c8a1f8df8b4af0c00df585f059bacf0
mfkmye <1720020157@qq.com>
2019-10-28 15:29:38 +0800

    update

---
+++ js/search.js
@@ -0,0 +1,1 @@
+//https://suggest.taobao.com/sug?code=utf-8&q=aaaa&_ksTS=1572247331313_3090&callback=jsonp3091&k=1&area=c2c&bucketid=2
---
+++ js/vue_youhaohuo.js
@@ -0,0 +1,17 @@
+$.ajax({
+    type: "POST",
+    url: "./data/youhaohuo.json",
+    dataType: "json",
+}).then(function (obj) {
+    var data1 = obj[0];
+    var data2 = obj[1];
+    var youhaohuo = new Vue({
+        el: "#youhaohuo",
+        data: data1
+    });
+    var aiguangjie = new Vue({
+        el: "#aiguangjie",
+        data: data2
+    });
+    console.log(youhaohuo.$data);
+});
